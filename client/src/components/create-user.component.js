636cb07a249d29cde0f11c5f59cb54ae18dd1795
RustamAV <rustam.alashrafov@gmail.com>
2019-09-18 23:36:57 +0200

    Implemented Create Exercise Log

---
+++ client/src/components/create-user.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+ 
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+      </div>
+    )
+  }
+}
---
+++ client/src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+    <div>
+      <h3>Exercises List Component</h3>
+    </div>
+    )
+  }
+}
